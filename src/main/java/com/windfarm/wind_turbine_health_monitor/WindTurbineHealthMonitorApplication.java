cf85d33e1f9262069e648206454b4b6515d5fac8
zadmin <zadmin@vm-lab173-26-553.asia-south1-a.c.niit-stackroute-2.internal>
2026-01-31 18:43:04 +0530

    First Commit

---
+++ src/main/java/com/windfarm/wind_turbine_health_monitor/WindTurbineHealthMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.windfarm.wind_turbine_health_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindTurbineHealthMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindTurbineHealthMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/windfarm/wind_turbine_health_monitor/WindTurbineHealthMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.windfarm.wind_turbine_health_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindTurbineHealthMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
