70e605a03c15ee1ded9c1ec49597e8b4942555c8
zadmin <zadmin@vm-lab173-26-553.asia-south1-a.c.niit-stackroute-2.internal>
2026-02-02 11:07:00 +0530

    Reafactored folder structure

---
+++ src/main/java/com/windfarm/wind_turbine_health_monitor/exception/ResourceNotFoundException.java
@@ -0,0 +1,4 @@
+package com.windfarm.wind_turbine_health_monitor.exception;
+
+public class ResourceNotFoundException {
+}
---
+++ src/main/java/com/windfarm/wind_turbine_health_monitor/service/AggregationService.java
@@ -0,0 +1,4 @@
+package com.windfarm.wind_turbine_health_monitor.service;
+
+public class AggregationService {
+}
